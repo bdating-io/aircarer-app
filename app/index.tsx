635e2a70b16a155e416a50d42629ede7e1401e95
EdwinHuang000 <103179293+EdwinHuang000@users.noreply.github.com>
2025-01-07 16:10:28 +1100

    start -template-config

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import { View, Text } from "react-native";
+
+const Home = () => {
+  return (
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="text-black text-5xl font-bold text-center">
+        Aircarer
+      </Text>
+    </View>
+  );
+};
+
+export default Home;
