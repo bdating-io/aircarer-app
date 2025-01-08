734c08f0ab72cd92ef2a4d97078737b66a0cd644
EdwinHuang000 <103179293+EdwinHuang000@users.noreply.github.com>
2025-01-08 14:04:52 +1100

    aadding some taiwind color and fonts

--- app/index.tsx
+++ app/index.tsx
@@ -3,7 +3,7 @@ import { View, Text } from "react-native";
 const Home = () => {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-black text-5xl font-bold text-center">
+      <Text className="text-black text-xl font-bold text-center">
         Aircarer
       </Text>
     </View>
