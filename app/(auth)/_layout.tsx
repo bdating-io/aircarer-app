eba5647493c697e3dedc918bd0e9566cabcb6016
EdwinHuang000 <103179293+EdwinHuang000@users.noreply.github.com>
2025-02-04 10:29:52 +1100

    expo router

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -10,8 +10,6 @@ function RootLayoutNav() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="(pages)" options={{ headerShown: false }} />
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" />
     </Stack>
   );
