de7a9e28a7c873521b13d7c75d8f1b7199ae8b8b
EdwinHuang000 <103179293+EdwinHuang000@users.noreply.github.com>
2025-02-06 16:47:21 +1100

    save it

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,84 +9,10 @@ export default function RootLayout() {
 function RootLayoutNav() {
   return (
     <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="(pages)" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="+not-found" />
-
-      <Stack.Screen
-        name="pages/taskPreparation/taskPrepare.tsx"
-        options={{ headerShown: true }}
-      />
-      <Stack.Screen
-        name="pages/profile/welcome.tsx"
-        options={{ headerShown: true }}
-      />
-
-      <Stack.Screen
-        name="pages/taskPreparation/taskAccept.tsx"
-        options={{ headerShown: true }}
-      />
-      <Stack.Screen
-        name="pages/taskPreparation/taskDetail.tsx"
-        options={{ headerShown: true }}
-      />
-      <Stack.Screen
-        name="pages/taskPreparation/taskHeading.tsx"
-        options={{ headerShown: true }}
-      />
-      <Stack.Screen
-        name="pages/authentication/home.tsx"
-        options={{ headerShown: true }}
-      />
-      <Stack.Screen
-        name="pages/authentication/profile.tsx"
-        options={{ headerShown: true }}
-      />
-      <Stack.Screen
-        name="pages/authentication/login.tsx"
-        options={{ headerShown: true }}
-      />
-
-      <Stack.Screen
-        name="pages/authentication/signup.tsx"
-        options={{ headerShown: true }}
-      />
-      <Stack.Screen
-        name="pages/profile/userTerms.tsx"
-        options={{ headerShown: true }}
-      />
-      <Stack.Screen name="(tabs)/home" options={{ headerShown: true }} />
-      <Stack.Screen
-        name="pages/profile/userProfile.tsx"
-        options={{ headerShown: true }}
-      />
-      <Stack.Screen
-        name="pages/profile/propertyList.tsx"
-        options={{ headerShown: true }}
-      />
-      <Stack.Screen
-        name="pages/profile/houseOwner.tsx"
-        options={{ headerShown: true }}
-      />
-      <Stack.Screen
-        name="pages/profile/cleanerProfile/cleanerProfile.tsx"
-        options={{ headerShown: true }}
-      />
-      <Stack.Screen
-        name="pages/profile/cleanerProfile/workingArea.tsx"
-        options={{ headerShown: true }}
-      />
-      <Stack.Screen
-        name="pages/profile/cleanerProfile/experience.tsx"
-        options={{ headerShown: true }}
-      />
-      <Stack.Screen
-        name="pages/profile/cleanerProfile/pricing.tsx"
-        options={{ headerShown: true }}
-      />
-      <Stack.Screen
-        name="pages/profile/cleanerProfile/skills.tsx"
-        options={{ headerShown: true }}
-      />
     </Stack>
   );
 }
