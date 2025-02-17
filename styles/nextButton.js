200b574241f914f9fe732111f674a4f93a4312a6
Quuaso <eibe0423@gmail.com>
2025-02-17 23:12:03 +1100

    second edition of merging
    updated utils
    added components, styles, mockData

---
+++ styles/nextButton.js
@@ -0,0 +1,28 @@
+// CustomButton.js
+import React from "react";
+import { TouchableOpacity, Text, StyleSheet } from "react-native";
+
+const NextButton = ({ title, onPress, style }) => {
+  return (
+    <TouchableOpacity style={[styles.button, style]} onPress={onPress}>
+      <Text style={styles.buttonText}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  button: {
+    backgroundColor: "#4E89CE",
+    padding: 15,
+    borderRadius: 8,
+    alignItems: "center",
+    marginTop: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    fontWeight: "bold",
+  },
+});
+
+export default NextButton;
