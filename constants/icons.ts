09fb48988ceaf2c7fa9fc54fe72ce820b4778f25
EdwinHuang000 <103179293+EdwinHuang000@users.noreply.github.com>
2025-02-26 13:27:10 +1100

    update icons

--- constants/icons.ts
+++ constants/icons.ts
@@ -1,8 +1,8 @@
 export const icons = {
-  home: require("../assets/icons/home.png"),
-  chat: require("../assets/icons/chat.png"),
-  list: require("../assets/icons/list.png"),
-  profile: require("../assets/icons/profile.png"),
+  home: require('../assets/icons/home.png'),
+  chat: require('../assets/icons/chat.png'),
+  list: require('../assets/icons/list.png'),
+  profile: require('../assets/icons/profile.png')
 };
 
 export default icons;
