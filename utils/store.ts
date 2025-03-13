797f075ae99f540f90b33d17ce296edae9caca8a
jojo99bd <jojo99@bdating.io>
2025-03-13 16:10:18 +1100

    fix build & store

--- utils/store.ts
+++ utils/store.ts
@@ -12,11 +12,26 @@ interface Profile {
 interface Store {
   myProfile: Profile | null;
   setMyProfile: (profile: Profile | null) => void;
+
+  mySession: any;
+  setMySession: (newSession: any) => void;
+
+  myAddress: any;
+  setMyAddress: (newAddress: any) => void;
+
+  myWorkPreference: any;
+  setMyWorkPreference: (newWorkPreference: any) => void;
 }
 
 const useStore = create<Store>((set) => ({
   myProfile: null,
   setMyProfile: (profile) => set({ myProfile: profile }),
+  mySession: {}, 
+  setMySession: (newSession: any) => set({ mySession: newSession }),  
+  myWorkPreference: {},
+  setMyAddress: (newAddress: any) => set({ myAddress: newAddress }),
+  myAddress: {},
+  setMyWorkPreference: (newPreference: any) => set({ myWorkPreference: newPreference }),
 }));
 
 export default useStore; 
