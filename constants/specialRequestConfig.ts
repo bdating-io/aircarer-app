1da276e3ca88198b8841345548f982b7398457ee
Quuaso <eibe0423@gmail.com>
2025-03-20 16:53:15 +1100

    updated price configuration

---
+++ constants/specialRequestConfig.ts
@@ -0,0 +1,93 @@
+export const SPECIAL_REQUEST_CONFIG = [
+    {
+      id: "stairCarpetCleaning",
+      label: "Stair Carpet Cleaning",
+      priceType: "flat",
+      price: 50, 
+    },
+    {
+      id: "ovenCleaning",
+      label: "Oven Cleaning",
+      priceType: "flat",
+      price: 60, 
+    },
+    {
+      id: "microwaveCleaning",
+      label: "Microwave Cleaning",
+      priceType: "flat",
+      price: 15, 
+    },
+    {
+      id: "fridgeCleaning",
+      label: "Fridge Cleaning",
+      priceType: "flat",
+      price: 40, 
+    },
+    {
+      id: "washingMachineCleaning",
+      label: "Washing Machine Cleaning",
+      priceType: "flat",
+      price: 40, 
+    },
+    {
+      id: "blindsCleaning",
+      label: "Blinds Cleaning",
+      priceType: "perItem",
+      price: 20, 
+    },
+    {
+      id: "glassCleaning",
+      label: "Glass Cleaning",
+      priceType: "perItem",
+      price: 15, 
+    },
+    {
+      id: "windowScreenCleaning",
+      label: "Window Screen Cleaning",
+      priceType: "perItem",
+      price: 15, 
+    },
+    {
+      id: "airconFilterCleaning",
+      label: "Aircon Filter Cleaning",
+      priceType: "perItem",
+      price: 15, 
+    },
+    {
+      id: "wallMouldRemoval",
+      label: "Wall Mould Removal",
+      priceType: "perItem",
+      price: 50, 
+    },
+    {
+      id: "wallStainRemoval",
+      label: "Wall Stain Removal",
+      priceType: "perItem",
+      price: 20, 
+    },
+    {
+      id: "petFurCleaning",
+      label: "Pet Fur Cleaning",
+      priceType: "flat",
+      price: 80, 
+    },
+    {
+      id: "balconyGarageCourtyardCleaning",
+      label: "Balcony / Garage / Courtyard",
+      priceType: "flat",
+      price: 20, 
+    },
+    {
+      id: "garbagePackaging",
+      label: "Garbage Packaging",
+      priceType: "perHour",
+      price: 50, 
+    },
+    {
+      id: "garbageDisposal",
+      label: "Garbage Disposal",
+      priceType: "perCubicMeter",
+      price: 150, 
+    },
+  ];
+  
